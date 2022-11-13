62162158dfbb899c967f8496473aa9dbd30eabb1
Edu Bastos Ramos <112495582+Eduramoss@users.noreply.github.com>
2022-11-12 21:33:41 -0300

    atualização

--- html/a.js
+++ html/a.js
@@ -1,10 +1,23 @@
 
-
-$('.carousel').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-  });
+4
+5
+6
+7
+8
+9
+10
+11
+12
+var $simpleCarousel = document.querySelector(".js-carousel--simple");
+ 
+new Glider($simpleCarousel, {
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  draggable: true,
+  dots: ".js-carousel--simple-dots",
+  arrows: {
+    prev: ".js-carousel--simple-prev",
+    next: ".js-carousel--simple-next",
+  },
+});
   
